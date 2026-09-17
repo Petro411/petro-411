4c41de79cd23f0f676fac60785392ed41d099842
Devhasnain <hasnainalam1166@gmail.com>
2026-09-17 16:16:34 +0500

    #fix added update bulk mineral api

--- src/pages/api/admin/mineral/update-bulk.ts
+++ src/pages/api/admin/mineral/update-bulk.ts
@@ -1,17 +1,21 @@
 import MineralOwner from "@/lib/mongodb/models/MineralOwner";
 import { withMethod } from "@/lib/middlewares/withMethod";
+import { withCors } from "@/lib/middlewares/withCors";
+import { withAuth } from "@/lib/middlewares/withAuth";
 import { dbConnect } from "@/lib/mongodb/dbConnect";
 
 
 async function handler(req: any, res: any) {
     try {
-        const {skip,limit} = req.query;
+        const { skip, limit } = req.query;
         await dbConnect();
         let updatedcounties = await MineralOwner.find({
             "counties.0": { $exists: true },
             "state.code": { $exists: true },
             "state.name": { $exists: true },
-        }).select(['counties']).lean().skip(Number(skip)).limit(Number(limit));
+            countiesNormalized: { $exists: false }
+
+        }).select(['counties', 'countiesNormalized']).lean().skip(Number(skip)).limit(Number(limit));
 
         let updates = updatedcounties.map((item) => {
             return {
@@ -23,11 +27,11 @@ async function handler(req: any, res: any) {
         });
 
         for (let i = 0; i < updates.length; i++) {
-             await MineralOwner.findByIdAndUpdate(updates[i]?._id, { 
+            await MineralOwner.findByIdAndUpdate(updates[i]?._id, {
                 countiesNormalized: updates[i].counties.map((c: string) =>
                     c.replace(/\s*County\s*$/i, "").trim().toLowerCase()
                 )
-             })
+            })
         }
 
         return res.status(200).json({ success: true });
@@ -40,4 +44,4 @@ async function handler(req: any, res: any) {
     }
 }
 
-export default withMethod(handler, ['GET']);
+export default withCors(withAuth(withMethod(handler, ["PUT"])));
